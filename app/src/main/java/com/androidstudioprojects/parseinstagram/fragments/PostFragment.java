105e93ef0590ed4762bf4bc9a32a887c568d99b5
mflynn19 <meghan.flynn@theflynn.org>
2019-11-23 03:07:27 -0500

    fixed refresh

--- app/src/main/java/com/androidstudioprojects/parseinstagram/fragments/PostFragment.java
+++ app/src/main/java/com/androidstudioprojects/parseinstagram/fragments/PostFragment.java
@@ -11,6 +11,7 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
 import com.androidstudioprojects.parseinstagram.Post;
 import com.androidstudioprojects.parseinstagram.PostsAdapter;
@@ -26,6 +27,7 @@ public class PostFragment extends Fragment {
     private RecyclerView rvPosts;
     protected PostsAdapter adapter;
     protected List<Post> mPosts;
+    private SwipeRefreshLayout swipeContainer;
 
     public static final String TAG = "PostFragment";
 
@@ -43,11 +45,25 @@ public class PostFragment extends Fragment {
         adapter = new PostsAdapter(getContext(), mPosts);
         rvPosts.setAdapter(adapter);
         rvPosts.setLayoutManager(new LinearLayoutManager(getContext()));
+
+        swipeContainer = (SwipeRefreshLayout) view.findViewById(R.id.swipeContainer);
+        // Setup refresh listener which triggers new data loading
+        swipeContainer.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                queryPosts();
+            }
+        });
+        // Configure the refreshing colors
+        swipeContainer.setColorSchemeResources(android.R.color.holo_blue_bright,
+                android.R.color.holo_green_light,
+                android.R.color.holo_orange_light,
+                android.R.color.holo_red_light);
         
         queryPosts();
     }
 
-    protected void queryPosts() {
+    public void queryPosts() {
         ParseQuery<Post> postQuery = new ParseQuery<Post>(Post.class);
         postQuery.include(Post.KEY_USER);
         postQuery.setLimit(20);
@@ -62,7 +78,9 @@ public class PostFragment extends Fragment {
                 }
                 mPosts.addAll(posts);
                 adapter.notifyDataSetChanged();
-
+                adapter.clear();
+                adapter.addAll(posts);
+                swipeContainer.setRefreshing(false);
                 for (int i = 0; i < posts.size(); i++){
                     Post post= posts.get(i);
                     Log.d(TAG, "Post " + post.getDescription() + ", username " + post.getUser().getUsername());
